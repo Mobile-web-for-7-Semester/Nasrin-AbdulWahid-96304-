3b65ddefacb9a84e4b03f14c1f135ae78ebd88a7
NasrinHaidari <nasrinhaidari786@gmail.com>
2021-02-02 20:15:29 +0430

    make contactThumnail component for profile screen

---
+++ components/contactThumnail.js
@@ -0,0 +1,41 @@
+import React from 'react'
+// building component from react
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    StyleSheet
+} from 'react-native'
+
+import colors from '../utils/colors'
+import Avatar from './Avatar';
+// function base component
+export default function ContactListItem({name, phone}){
+   return(
+       <View style={styles.thumnailContainer}>
+           <Avatar name={name} size={90}/>
+           <Text style={styles.name}>
+               {name}
+           </Text>
+           <Text style={styles.phone}>
+               {phone}
+           </Text>
+       </View>
+   )
+}
+
+const styles = StyleSheet.create({
+    thumnailContainer: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    name: {
+        color: colors.white,
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    phone: {
+        color: colors.white,
+        fontSize: 16
+    }
+})
