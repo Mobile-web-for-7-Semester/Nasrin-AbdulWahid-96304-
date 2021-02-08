ae79f29a469ef5318c29c83cd685bd2eb05c6aa1
NasrinHaidari <nasrinhaidari786@gmail.com>
2021-02-08 15:58:31 +0430

    use SQLite to conect the database and make a contact table

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
 import { MaterialIcons } from "@expo/vector-icons";
 import { NavigationContainer } from '@react-navigation/native'; // for navigate between screen
 import { createStackNavigator } from "@react-navigation/stack";
+import * as SQLite from 'expo-sqlite';
 // becuase we use createStackNavigator in many time, for that we make a const
 const Stack= createStackNavigator();
+const db = SQLite.openDatabase('contacts.db')
 
 import Contact from "./screens/Contacts";
 import CreateContact from "./screens/CreateContacts";
 import Profile from "./screens/Profile";
 import SearchScreen from './screens/SearchScreen'
 export default function App() {
+  // for using hook
+  useEffect(()=> {
+    db.transaction(tx=>{
+      tx.executeSql('create table if not exists contact(id integer primary key autoincrement, name text, phone text, email text);',[],()=>console.log('table created successfully'));
+    })
+  })
+
   // for moving one screen to another screen, we use navigation with navigate method
   return (
     <NavigationContainer>
