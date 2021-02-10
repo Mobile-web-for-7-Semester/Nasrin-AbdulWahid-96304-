46a03e5fa62ba50e44b66cbd361d8d4c1ce40e9a
NasrinHaidari <nasrinhaidari786@gmail.com>
2021-02-10 18:02:05 +0430

    contacts screen renderd select all data from contacts table and show that

--- screens/Contacts.js
+++ screens/Contacts.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 // building component from react
 import {
     View,
@@ -8,32 +8,44 @@ import {
     StyleSheet
 } from 'react-native'
 import { Feather } from '@expo/vector-icons';
+import * as SQLite from "expo-sqlite";
+
 import colors from '../utils/colors'
 import ContactListItem from '../components/contactListItem'
 
-const contacts = [
-    { id:'1', name:'Nasrin', phone:'8489458', email:'nasrin@gmail.com' },
-    { id:'2', name:'Nargis', phone:'8466758', email:'nargis@gmail.com' },
-    { id:'3', name:'Ryhana', phone:'8494458', email:'ryhana@gmail.com' },
-    { id:'4', name:'fatemeh', phone:'8489458', email:'fatemeh@gmail.com' },
-    { id:'5', name:'zahra', phone:'8489458', email:'zahra@gmail.com' },
-    { id:'6', name:'Marzia', phone:'8489458', email:'marzia@gmail.com' },
-    { id:'1', name:'Nasrin', phone:'8489458', email:'nasrin@gmail.com' },
-    { id:'2', name:'Nargis', phone:'8466758', email:'nargis@gmail.com' },
-    { id:'3', name:'Ryhana', phone:'8494458', email:'ryhana@gmail.com' }
-]
+const db= SQLite.openDatabase('contacts.db');
 
 
 // function base component
 export default function Contacts({navigation}){
+  const [contacts, setContacts] = useState([]); // => means contacts that show in flatList didn't show any thing at the first time
+
+  // when contacts screen renderd select all data from contacts table and show that ==> for that use from useEffect hook 
+  useEffect(()=>{
+    db.transaction((tx)=>{
+      tx.executeSql('SELECT * FROM contacts',[],(tx,{rows})=>{
+        //all data that we select com in rows and we need to array from object
+        var data=[];
+        for(var i = 0; i<rows.length; i++) {
+          data.push(rows[i]); 
+        }
+        //now we should change our state, at the first state is null contact
+        // after that render data in (1 sec)
+        setContacts(data);
+      })
+    })
+  })
+
   return(
     <View>
-      <FlatList
+      { contacts.length > 0 ? <FlatList
       data={contacts}
       renderItem={({item}) => { 
         return <ContactListItem name={item.name} phone={item.phone} onPress={()=> navigation.navigate('Profile',{item:item})}/>
       }}
-      />
+      />: <View>
+            <Text>There is no contact</Text>
+        </View>}
       <TouchableOpacity style={styles.floatButton} onPress={()=> navigation.navigate('CreateContact')}>
         <Text>
           <Feather name="plus" size={28} color="white"/>
